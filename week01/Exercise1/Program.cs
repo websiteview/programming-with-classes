91366f9c747e8b42e89a46e54455a2e50d3d82de
byui-professional <men22027@byui.edu>
2025-07-04 20:02:33 -0600

    Added a comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,7 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
-
+        
+        //Ask the user for their name.
         Console.Write("What is your first name? ");
         string firstname = Console.ReadLine();
         Console.Write("What is your last name? ");
