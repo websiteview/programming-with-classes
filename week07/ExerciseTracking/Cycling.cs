d8e526d36ebee12d9547263b67386a8260261be1
byui-professional <men22027@byui.edu>
2025-08-11 20:23:31 -0600

    ExerciseTracker done

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,16 @@
+public class Cycling : Activity
+{
+    private double _speed;
+
+    public Cycling(string date, int minutes, double speed)
+        : base(date, minutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance() => (_speed * GetMinutes()) / 60;
+
+    public override double GetSpeed() => _speed;
+
+    public override double GetPace() => 60 / _speed;
+}
